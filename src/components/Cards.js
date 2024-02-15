700bd4cec1d4d2609d11b2c1b6bad923ed8fe402
Subhankar chakrborty <subhankarchakrborty@gmail.com>
2024-02-15 12:32:58 +0530

    image cart added

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import Cardsdata from "../components/CardData";
+import "./style.css";
 
 const Cards = () => {
-  return <div>Cards</div>;
+  const [data, setData] = useState(Cardsdata);
+  // console.log(data);
+
+  return (
+    <div className="container mt-3">
+      <h2 className="text-center">Add to Cart Projects</h2>
+      <div className="row d-flex justify-content-center align-item-center">
+        {data.map((element, id) => {
+          return (
+            <>
+              <Card
+                border="primary"
+                style={{ width: "22rem", border: "none" }}
+                className="mx-2 mt-4 card_style"
+              >
+                <Card.Img
+                  variant="top"
+                  src={element.imgdata}
+                  style={{ height: "15rem" }}
+                  className="mt-3"
+                />
+                <Card.Header>Header</Card.Header>
+                <Card.Body>
+                  <Card.Title>{element.rname}</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Cards;
